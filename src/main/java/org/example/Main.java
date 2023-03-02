2442099eb774d87b1004f9e5ef70fcd9f725a2f8
chener <49267211+chanosong@users.noreply.github.com>
2023-03-02 10:18:11 +0900

    feat: Add modify function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -36,7 +36,9 @@ public class Main {
                 System.out.println("----------------");
 
                 for (int i = cnt-1; i >=0; i-- ) {
-                    System.out.println(list.get(i).toString());
+                    if (list.get(i).isExist()) {
+                        System.out.println(list.get(i).toString());
+                    }
                 }
             }
 
@@ -44,13 +46,32 @@ public class Main {
                 String[] deletion = cmd.split("=");
                 int idx = Integer.valueOf(deletion[1]);
 
-                if (list.get(idx).isExist()) {
-                    list.get(idx).delete();
+                if (list.get(idx - 1).isExist()) {
+                    list.get(idx - 1).delete();
                     System.out.println(deletion[1] + "번 명언이 삭제되었습니다.");
                 }
                 else {
                     System.out.println(deletion[1] + "번 명언은 존재하지 않습니다.");
                 }
+            }
+
+            else if (cmd.contains("수정")) {
+                String[] modi = cmd.split("=");
+                int idx = Integer.valueOf(modi[1]);
+                String newContent;
+                String newAuthor;
+
+                System.out.print("명언(기존) : ");
+                System.out.println(list.get(idx - 1).getContent());
+                System.out.print("명언 : ");
+                newContent = sc.nextLine();
+
+                System.out.print("작가(기존) : ");
+                System.out.println(list.get(idx - 1).getAuthor());
+                System.out.print("작가 : ");
+                newAuthor = sc.nextLine();
+
+                list.get(idx - 1).modify(newAuthor, newContent);
             }
         }
     }
@@ -72,6 +93,14 @@ class Saying {
         return id + " / " + author + " / " + content;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
     public void delete() {
         author = null;
         content = null;
@@ -79,5 +108,10 @@ class Saying {
 
     public boolean isExist() {
         return !(author == null && content == null);
+    }
+
+    public void modify(String author, String content) {
+        this.author = author;
+        this.content = content;
     }
 }
